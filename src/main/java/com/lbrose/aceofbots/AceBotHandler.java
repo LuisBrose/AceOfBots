8230541345d9bec4139f41434060c377c4407a57
LuisBrose <luisbrose1@gmail.com>
2023-04-26 17:28:07 +0200

    Added Buttons
    *some buttons as an early prototype of the ui

--- src/main/java/com/lbrose/aceofbots/AceBotHandler.java
+++ src/main/java/com/lbrose/aceofbots/AceBotHandler.java
@@ -1,10 +1,35 @@
 package com.lbrose.aceofbots;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+
+import java.awt.*;
 
 public class AceBotHandler {
 
     public void play(SlashCommandInteractionEvent event) {
         event.reply("let's go").queue();
+
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setTitle("AceOfBots")
+                .setColor(Color.green);
+
+        MessageEmbed embed = builder.build();
+
+        event.getChannel().sendMessageEmbeds(embed)
+                .addActionRow(
+                        Button.primary("checkCall", "check/call"),
+                        Button.success("raise", "raise"),
+                        Button.danger("fold", "fold")
+                )
+                .addActionRow(
+                        Button.secondary("allIn", "all in"),
+                        Button.secondary("up", Emoji.fromUnicode("U+1F53A")),
+                        Button.secondary("down", Emoji.fromUnicode("U+1F53B"))
+                )
+                .queue();
     }
 }
